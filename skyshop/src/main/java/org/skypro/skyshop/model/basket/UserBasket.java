226d39880b06573c13bce0aac038532409aaa5f8
TabrizKakhramanov <tabriz9998@gmil.com>
2025-02-19 17:03:16 +0300

    Corrected mistake

--- skyshop/src/main/java/org/skypro/skyshop/model/basket/UserBasket.java
+++ skyshop/src/main/java/org/skypro/skyshop/model/basket/UserBasket.java
@@ -6,10 +6,18 @@ public class UserBasket {
     private List<BasketItem> basketItems;
     private int total;
 
+    public List<BasketItem> getBasketItems() {
+        return basketItems;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
     public UserBasket(List<BasketItem> basketItem){
         this.basketItems = basketItem;
         this.total = basketItem.stream().map(el->el.getProduct().getProductPrice()*el.getProductQuantity())
-                .reduce((a,e)->a+e).get();
+                .reduce((a,e)->a+e).orElse(0);
     }
 }
 
